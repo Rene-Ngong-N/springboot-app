e21c189892d80b909b0d43cc34ac236923540a99
harish <harish.sathyanarayan@ibm.com>
2024-07-25 14:39:58 -0400

    pushing sample springboot app for pipeline framework

---
+++ springboot-sample/src/main/java/springboot/first_app/App.java
@@ -0,0 +1,34 @@
+package springboot.first_app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class App
+
+
+{     
+	@Value("hello")
+	private String message;
+	
+
+	@RequestMapping("/")
+	String home() {
+		return message;
+	}
+
+	@RequestMapping("/display")
+	String home2() {
+		return message + " world";
+	}
+
+	public static void main( String[] args )
+	{
+		SpringApplication.run(App.class, args);
+	}
+}
